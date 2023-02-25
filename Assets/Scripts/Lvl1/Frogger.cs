9325c9dee83499384eb9818ffca4256f06925a1f
STudor1 <tudor122stefan@gmail.com>
2023-02-25 15:28:30 +0000

    Frogger faces the direction based on direction

--- Assets/Scripts/Lvl1/Frogger.cs
+++ Assets/Scripts/Lvl1/Frogger.cs
@@ -8,18 +8,22 @@ public class Frogger : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow)) 
         {
+            Rotation(0);
             Move(Vector3.up);
         } 
         else if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow)) 
         {
+            Rotation(180);
             Move(Vector3.down);
         }
         else if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow))
         {
+            Rotation(90);
             Move(Vector3.left);
         }
         else if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow))
         {
+            Rotation(-90);
             Move(Vector3.right);
         }
     }
@@ -27,5 +31,10 @@ public class Frogger : MonoBehaviour
     private void Move(Vector3 direction)
     {
         transform.position += direction;
+    }
+
+    private void Rotation(float rotation)
+    {
+        transform.rotation = Quaternion.Euler(0f, 0f, rotation);
     }
 }
