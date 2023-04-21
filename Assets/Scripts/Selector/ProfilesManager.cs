76bd5bd24e9dc1813e99e814606493c88f3e2cd3
STudor1 <tudor122stefan@gmail.com>
2023-04-21 14:23:21 +0100

    Working on displaying a new btn for profiles

--- Assets/Scripts/Selector/ProfilesManager.cs
+++ Assets/Scripts/Selector/ProfilesManager.cs
@@ -9,6 +9,8 @@ public class ProfilesManager : MonoBehaviour
 {
     private ProfileData[] profiles;
     private int totalProfiles;
+    [SerializeField] private GameObject buttonPrefab;
+    [SerializeField] private GameObject canvas;
 
     public void UpdateManager()
     {
@@ -23,8 +25,41 @@ public class ProfilesManager : MonoBehaviour
         
         //profiles = GetComponents<ProfileData>();
         Debug.Log("There are " + totalProfiles + " profiles");
-    }
 
+        //we need to do this whenever a new profile has been added 
+
+        //this creates a new button under our test profiles button
+        Vector3 pos = new Vector3(960, 540, 0);
+        Quaternion rot = Quaternion.Euler(0, 0, 0);
+        GameObject button = (GameObject)Instantiate(buttonPrefab);
+        button.transform.SetPositionAndRotation(pos, rot);
+        button.transform.SetParent(canvas.transform);//Setting button parent
+
+    }
+    public void UpdateManagerName(string username)
+    {
+        if (PlayerPrefs.GetInt("ID") == 0)
+        {
+            totalProfiles = PlayerPrefs.GetInt("ID");
+        }
+        else
+        {
+            totalProfiles = PlayerPrefs.GetInt("ID") - 1;
+        }
+
+        //profiles = GetComponents<ProfileData>();
+        Debug.Log("There are " + totalProfiles + " profiles");
+
+        //we need to do this whenever a new profile has been added 
+
+        //this creates a new button under our test profiles button
+        Vector3 pos = new Vector3(960, 540, 0);
+        Quaternion rot = Quaternion.Euler(0, 0, 0);
+        GameObject button = (GameObject)Instantiate(buttonPrefab);
+        button.transform.SetPositionAndRotation(pos, rot);
+        button.transform.SetParent(canvas.transform);//Setting button parent
+
+    }
     //Use this to reset everything
     public void Reset()
     {
