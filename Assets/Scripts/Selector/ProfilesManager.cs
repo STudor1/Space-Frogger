d7340a1e731adfb781433ed1f893839fb82deae3
STudor1 <tudor122stefan@gmail.com>
2023-04-22 14:10:33 +0100

    Creates buttons when profile added
    No name to button yet, button has no function either

--- Assets/Scripts/Selector/ProfilesManager.cs
+++ Assets/Scripts/Selector/ProfilesManager.cs
@@ -11,6 +11,44 @@ public class ProfilesManager : MonoBehaviour
     private int totalProfiles;
     [SerializeField] private GameObject buttonPrefab;
     [SerializeField] private GameObject canvas;
+    private int currentY;
+
+    private void Start()
+    {
+        if (PlayerPrefs.GetInt("ID") == 0)
+        {
+            totalProfiles = PlayerPrefs.GetInt("ID");
+        }
+        else
+        {
+            totalProfiles = PlayerPrefs.GetInt("ID") - 1;
+        }
+        //}
+        //else
+        //{
+        //    totalProfiles = PlayerPrefs.GetInt("ID") - 1;
+        //}
+
+        //profiles = GetComponents<ProfileData>();
+
+        if (totalProfiles != 0)
+        {
+            for (int i = 0; i < totalProfiles; i++)
+            {
+                Vector3 pos = new Vector3(960, 540 + (-120 * i), 0);
+                Quaternion rot = Quaternion.Euler(0, 0, 0);
+                GameObject button = (GameObject)Instantiate(buttonPrefab);
+                button.transform.SetPositionAndRotation(pos, rot);
+                button.transform.SetParent(canvas.transform);//Setting button parent
+                currentY = 540 + (-120 * i);
+            }
+        }
+        else
+        {
+            currentY = 540 + 120;
+        }
+    }
+
 
     public void UpdateManager()
     {
@@ -26,38 +64,39 @@ public class ProfilesManager : MonoBehaviour
         //profiles = GetComponents<ProfileData>();
         Debug.Log("There are " + totalProfiles + " profiles");
 
-        //we need to do this whenever a new profile has been added 
-
-        //this creates a new button under our test profiles button
-        Vector3 pos = new Vector3(960, 540, 0);
-        Quaternion rot = Quaternion.Euler(0, 0, 0);
-        GameObject button = (GameObject)Instantiate(buttonPrefab);
-        button.transform.SetPositionAndRotation(pos, rot);
-        button.transform.SetParent(canvas.transform);//Setting button parent
 
     }
     public void UpdateManagerName(string username)
     {
-        if (PlayerPrefs.GetInt("ID") == 0)
-        {
-            totalProfiles = PlayerPrefs.GetInt("ID");
-        }
-        else
-        {
-            totalProfiles = PlayerPrefs.GetInt("ID") - 1;
-        }
+        //if (PlayerPrefs.GetInt("ID") == 0)
+        //{
+        totalProfiles = PlayerPrefs.GetInt("ID");
+        //}
+        //else
+        //{
+        //    totalProfiles = PlayerPrefs.GetInt("ID") - 1;
+        //}
 
         //profiles = GetComponents<ProfileData>();
         Debug.Log("There are " + totalProfiles + " profiles " + username);
 
         //we need to do this whenever a new profile has been added 
 
-        //this creates a new button under our test profiles button
-        Vector3 pos = new Vector3(960, 540, 0);
-        Quaternion rot = Quaternion.Euler(0, 0, 0);
-        GameObject button = (GameObject)Instantiate(buttonPrefab);
-        button.transform.SetPositionAndRotation(pos, rot);
-        button.transform.SetParent(canvas.transform);//Setting button parent
+        if (totalProfiles < 4)
+        {
+            //this creates a new button under our test profiles button
+            Vector3 pos = new Vector3(960, currentY - 120, 0); //this is 0 for some reason next at y + 120
+            Quaternion rot = Quaternion.Euler(0, 0, 0);
+            GameObject button = (GameObject)Instantiate(buttonPrefab);
+            button.transform.SetPositionAndRotation(pos, rot);
+            button.transform.SetParent(canvas.transform);//Setting button parent
+            currentY = currentY - 120;
+        }
+        else
+        {
+            Debug.Log("Delete a profile to add another one");
+        }
+       
 
     }
     //Use this to reset everything
