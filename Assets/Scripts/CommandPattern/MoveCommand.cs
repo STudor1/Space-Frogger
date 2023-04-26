b7ab4a22e7eb8003a429c5cb30998ef88cdb9993
STudor1 <tudor122stefan@gmail.com>
2023-04-26 16:38:12 +0100

    Fixed a bug

--- Assets/Scripts/CommandPattern/MoveCommand.cs
+++ Assets/Scripts/CommandPattern/MoveCommand.cs
@@ -59,7 +59,39 @@ public class MoveCommand : Command
     {
         Vector3 destination = entity.transform.position - _direction;
 
-        _frogger.StartCoroutine(_frogger.Leap(destination));
+        Collider2D barrier = Physics2D.OverlapBox(destination, Vector2.zero, 0f, LayerMask.GetMask("Barrier"));
+        Collider2D platform = Physics2D.OverlapBox(destination, Vector2.zero, 0f, LayerMask.GetMask("Platform"));
+        Collider2D obstacle = Physics2D.OverlapBox(destination, Vector2.zero, 0f, LayerMask.GetMask("Obstacle"));
+
+        //This means that if there is a barrier on the next destination we return without doing the code after
+        if (barrier != null)
+        {
+            return;
+        }
+
+        if (platform != null)
+        {
+            entity.transform.SetParent(platform.transform); //attach our self to platform
+        }
+        else
+        {
+            entity.transform.SetParent(null); //detach from platform
+        }
+
+        if (obstacle != null && platform == null)
+        {
+            entity.transform.position = destination;
+            _frogger.Death();
+        }
+        else
+        {
+            if (destination.y > _farthestRow)
+            {
+                _farthestRow = destination.y;
+                _manager.AdvancedRow();
+            }
+            _frogger.StartCoroutine(_frogger.Leap(destination));
+        }
 
     }
 
