9acabc0616ef98e505bd15c4f998a92ef4950298
STudor1 <tudor122stefan@gmail.com>
2023-05-01 14:32:03 +0100

    Working on achievements

---
+++ Assets/Scripts/Achievements/AchievementService.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// This is the observer, and frogger is the subject
+/// </summary>
+public class AchievementService : MonoBehaviour
+{
+    private void OnEnable()
+    {
+        
+    }
+
+    private void OnDisable()
+    {
+        
+    }
+
+    private void UnlockAchievement()
+    {
+
+    }
+
+    private void CheckIfUnlocked()
+    {
+
+    }
+}
